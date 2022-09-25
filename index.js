308126a6a532bd59f1d1c839f9c515e9bc6f2cb5
rama <rama@packetai.co>
2022-09-25 19:37:20 +0200

    init azure logs forwarder

---
+++ index.js
@@ -0,0 +1,429 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+
+const https = require("https");
+
+const VERSION = "0.5.7";
+
+const STRING = "string";
+const STRING_ARRAY = "string-array";
+const JSON_OBJECT = "json-object";
+const JSON_ARRAY = "json-array";
+const BUFFER_ARRAY = "buffer-array";
+const JSON_STRING = "json-string";
+const JSON_STRING_ARRAY = "json-string-array";
+const INVALID = "invalid";
+
+const JSON_TYPE = "json";
+const STRING_TYPE = "string";
+
+const PAI_API_KEY = process.env.PAI_IID || '<PAI_IID>';
+const X_PAI_TOKEN = process.env.PAI_TOKEN || '<PAI_TOKEN>';
+const PAI_HTTP_URL = process.env.PAI_SITE || "vector-ingester-gcpdev.packetai.co";
+const PAI_HTTP_PORT = 443;
+const PAI_REQUEST_TIMEOUT_MS = 10000;
+const PAI_SOURCE = "azure";
+const PAI_CLUSTER_NAME = "azure";
+
+const MAX_RETRIES = 4; // max number of times to retry a single http request
+const RETRY_INTERVAL = 250; // amount of time (milliseconds) to wait before retrying request, doubles after every retry
+
+class Batcher {
+  constructor(context, maxItemSizeBytes, maxBatchSizeBytes, maxItemsCount) {
+    this.maxItemSizeBytes = maxItemSizeBytes;
+    this.maxBatchSizeBytes = maxBatchSizeBytes;
+    this.maxItemsCount = maxItemsCount;
+  }
+
+  batch(items) {
+    var batches = [];
+    var batch = [];
+    var sizeBytes = 0;
+    var sizeCount = 0;
+    for (const item of items) {
+      var itemSizeBytes = this.getSizeInBytes(item);
+      if (
+        sizeCount > 0 &&
+        (sizeCount >= this.maxItemsCount ||
+          sizeBytes + itemSizeBytes > this.maxBatchSizeBytes)
+      ) {
+        batches.push(batch);
+        batch = [];
+        sizeBytes = 0;
+        sizeCount = 0;
+      }
+      // all items exceeding maxItemSizeBytes are dropped here
+      if (itemSizeBytes <= this.maxItemSizeBytes) {
+        batch.push(item);
+        sizeBytes += itemSizeBytes;
+        sizeCount += 1;
+      }
+    }
+
+    if (sizeCount > 0) {
+      batches.push(batch);
+    }
+    return batches;
+  }
+
+  getSizeInBytes(string) {
+    if (typeof string !== "string") {
+      string = JSON.stringify(string);
+    }
+    return Buffer.byteLength(string, "utf8");
+  }
+}
+
+class HTTPClient {
+  constructor(context) {
+    this.context = context;
+    this.httpOptions = {
+      hostname: PAI_HTTP_URL,
+      port: PAI_HTTP_PORT,
+      path: "/azure/log",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-PAI-TOKEN": X_PAI_TOKEN,
+        "X-PAI-IID": PAI_API_KEY,
+      },
+      timeout: PAI_REQUEST_TIMEOUT_MS,
+    };
+    this.batcher = new Batcher(this.context, 256 * 1000, 4 * 1000 * 1000, 400);
+  }
+
+  async sendAll(records) {
+    let batches = this.batcher.batch(records);
+    let promises = [];
+    for (const element of batches) {
+      promises.push(this.send(element));
+    }
+    return await Promise.all(
+      promises.map((p) => p.catch((e) => this.context.log.error(e)))
+    );
+  }
+
+  isStatusCodeValid(statusCode) {
+    return statusCode >= 200 && statusCode <= 299;
+  }
+
+  shouldStatusCodeRetry(statusCode) {
+      this.context.log("statusCode: ",statusCode);
+    // don't retry 4xx responses
+    return (
+      !this.isStatusCodeValid(statusCode) &&
+      (statusCode < 400 || statusCode > 499)
+    );
+  }
+
+  send(record) {
+    let numRetries = MAX_RETRIES;
+    let retryInterval = RETRY_INTERVAL;
+    return new Promise((resolve, reject) => {
+      const sendRequest = (options, record) => {
+        const retryRequest = (errMsg) => {
+          if (numRetries === 0) {
+            return reject(errMsg);
+          }
+          this.context.log.warn(
+            `Unable to send request, with error: ${errMsg}. Retrying ${numRetries} more times`
+          );
+          numRetries--;
+          retryInterval *= 2;
+          setTimeout(() => {
+            sendRequest(options, record);
+          }, retryInterval);
+        };
+        const req = https
+          .request(options, (resp) => {
+            if (this.isStatusCodeValid(resp.statusCode)) {
+              resolve(true);
+            } else if (this.shouldStatusCodeRetry(resp.statusCode)) {
+              retryRequest(`invalid status code ${resp.statusCode}`);
+            } else {
+              reject(`invalid status code ${resp.statusCode}`);
+            }
+          })
+          .on("error", (error) => {
+            retryRequest(error.message);
+          })
+          .on("timeout", () => {
+            req.destroy();
+            retryRequest(`request timed out after ${PAI_REQUEST_TIMEOUT_MS}ms`);
+          });
+        req.write(JSON.stringify(record));
+        req.end();
+      };
+      sendRequest(this.httpOptions, record);
+    });
+  }
+}
+
+class EventhubLogHandler {
+  constructor(context) {
+    this.context = context;
+    this.records = [];
+  }
+
+  findSplitRecords(record, fields) {
+    var tempRecord = record;
+    for (const element of fields) {
+      // loop through the fields to find the one we want to split
+      var fieldName = element;
+      if (
+        tempRecord[fieldName] === undefined ||
+        tempRecord[fieldName] === null
+      ) {
+        // if the field is null or undefined, return
+        return null;
+      } else {
+        // there is some value for the field
+        try {
+          // if for some reason we can't index into it, return null
+          tempRecord = tempRecord[fieldName];
+        } catch {
+          return null;
+        }
+      }
+    }
+    return tempRecord;
+  }
+
+  formatLog(messageType, record) {
+    if (messageType == JSON_TYPE) {
+      let originalRecord = this.addTagsToJsonLog(record);
+      this.records.push(originalRecord);
+    } else {
+      record = this.addTagsToStringLog(record);
+      this.records.push(record);
+    }
+  }
+
+  handleLogs(logs) {
+    let logsType = this.getLogFormat(logs);
+    this.context.log.info('Log format type: ', logsType);
+    switch (logsType) {
+      case STRING:
+        this.formatLog(STRING_TYPE, logs);
+        break;
+      case JSON_STRING:
+        logs = JSON.parse(logs);
+        this.formatLog(JSON_TYPE, logs);
+        break;
+      case JSON_OBJECT:
+        this.formatLog(JSON_TYPE, logs);
+        break;
+      case STRING_ARRAY:
+        logs.forEach((log) => this.formatLog(STRING_TYPE, log));
+        break;
+      case JSON_ARRAY:
+        this.handleJSONArrayLogs(logs, JSON_ARRAY);
+        break;
+      case BUFFER_ARRAY:
+        this.handleJSONArrayLogs(logs, BUFFER_ARRAY);
+        break;
+      case JSON_STRING_ARRAY:
+        this.handleJSONArrayLogs(logs, JSON_STRING_ARRAY);
+        break;
+      case INVALID:
+        this.context.log.error("Log format is invalid: ", logs);
+        break;
+      default:
+        this.context.log.error("Log format is invalid: ", logs);
+        break;
+    }
+    return this.records;
+  }
+
+  handleJSONArrayLogs(logs, logsType) {
+    for (const element of logs) {
+      let message = element;
+      if (logsType == JSON_STRING_ARRAY) {
+        try {
+          message = JSON.parse(message);
+        } catch (err) {
+          this.context.log.warn("log is malformed json, sending as string");
+          this.formatLog(STRING_TYPE, message);
+          continue;
+        }
+      }
+      // If the message is a buffer object, the data type has been set to binary.
+      if (logsType == BUFFER_ARRAY) {
+        try {
+          message = JSON.parse(message.toString());
+        } catch (err) {
+          this.context.log.warn("log is malformed json, sending as string");
+          this.formatLog(STRING_TYPE, message.toString());
+          continue;
+        }
+      }
+      if (message.records != undefined) {
+        message.records.forEach((message) =>
+          this.formatLog(JSON_TYPE, message)
+        );
+      } else {
+        this.formatLog(JSON_TYPE, message);
+      }
+    }
+  }
+
+  getLogFormat(logs) {
+    if (typeof logs === "string") {
+      if (this.isJsonString(logs)) {
+        return JSON_STRING;
+      }
+      return STRING;
+    }
+    if (!Array.isArray(logs) && typeof logs === "object" && logs !== null) {
+      return JSON_OBJECT;
+    }
+    if (!Array.isArray(logs)) {
+      return INVALID;
+    }
+    if (Buffer.isBuffer(logs[0])) {
+      return BUFFER_ARRAY;
+    }
+    if (typeof logs[0] === "object") {
+      return JSON_ARRAY;
+    }
+    if (typeof logs[0] === "string") {
+      if (this.isJsonString(logs[0])) {
+        return JSON_STRING_ARRAY;
+      } else {
+        return STRING_ARRAY;
+      }
+    }
+    return INVALID;
+  }
+
+  isJsonString(record) {
+    try {
+      JSON.parse(record);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  addTagsToJsonLog(record) {
+    record["paisource"] = PAI_SOURCE;
+    this.addExtractMetadataToResource(record);
+    return record;
+  }
+
+  addTagsToStringLog(stringLog) {
+    var jsonLog = { message: stringLog };
+    return this.addTagsToJsonLog(jsonLog);
+  }
+
+  createResourceIdArray(record) {
+    // Convert the resource ID in the record to an array, handling beginning/ending slashes
+    var recordResourceId = record.resourceId || record._ResourceId;
+    var resourceId = recordResourceId.toLowerCase().split("/");
+    if (resourceId[0] === "") {
+      resourceId = resourceId.slice(1);
+    }
+    if (resourceId[resourceId.length - 1] === "") {
+      resourceId.pop();
+    }
+    return resourceId;
+  }
+
+  isSource(resourceIdPart) {
+    // Determine if a section of a resource ID counts as a "source," in our case it means it starts with 'microsoft.'
+    return resourceIdPart.startsWith("microsoft.");
+  }
+
+  formatSourceType(sourceType) {
+    return sourceType.replace("microsoft.", "azure.");
+  }
+
+  addExtractMetadataToResource(record) {
+    record['packetai.cluster_name'] = PAI_CLUSTER_NAME;
+    var recordResourceId = record.resourceId || record._ResourceId;
+    if (
+      recordResourceId === undefined ||
+      typeof recordResourceId !== "string"
+    ) {
+      return;
+    }
+    var resourceId = this.createResourceIdArray(record);
+
+    if (resourceId[0] === "subscriptions") {
+      if (resourceId.length > 1) {
+        record["subscription_id"] = record["subscription_id"] || resourceId[1];
+        if (resourceId.length == 2) {
+          record["paisource"] = "azure.subscription";
+          return;
+        }
+      }
+      if (resourceId.length > 3) {
+        if (resourceId[2] === "providers" && this.isSource(resourceId[3])) {
+          // handle provider-only resource IDs
+          record["paisource"] = this.formatSourceType(resourceId[3]);
+        } else {
+          record["resource_group"] = record["resource_group"] || resourceId[3];
+          if (resourceId.length == 4) {
+            record["paisource"] = "azure.resourcegroup";
+            return;
+          }
+        }
+      }
+      if (resourceId.length > 5 && this.isSource(resourceId[5])) {
+        record["paisource"] = this.formatSourceType(resourceId[5]);
+      }
+    } else if (resourceId[0] === "tenants") {
+      if (resourceId.length > 3 && resourceId[3]) {
+        record["tenant"] = record["tenant"] || resourceId[1];
+        record["paisource"] = this.formatSourceType(resourceId[3]).replace(
+          "aadiam",
+          "activedirectory"
+        );
+      }
+    }
+  }
+}
+
+module.exports = async function (context, eventHubMessages) {
+  if (!PAI_API_KEY || PAI_API_KEY === "<PAI_API_KEY>") {
+    context.log.error(
+      "You must configure your PAI_IID key before starting this function (see ## Parameters section)"
+    );
+    return;
+  }
+  if (!X_PAI_TOKEN || X_PAI_TOKEN === "<X_PAI_TOKEN>") {
+    context.log.error(
+      "You must configure your PAI_TOKEN key before starting this function (see ## Parameters section)"
+    );
+    return;
+  }
+  try {
+    var handler = new EventhubLogHandler(context);
+    var parsedLogs = handler.handleLogs(eventHubMessages);
+  } catch (err) {
+    context.log.error("Error raised when parsing logs: ", err);
+    throw err;
+  }
+  var results = await new HTTPClient(context).sendAll(parsedLogs);
+
+  if (results.every((v) => v === true) !== true) {
+    context.log.error(
+      "Some messages were unable to be sent. See other logs for details."
+    );
+  }
+};
+
+module.exports.forTests = {
+  EventhubLogHandler,
+  Batcher,
+  constants: {
+    STRING,
+    STRING_ARRAY,
+    JSON_OBJECT,
+    JSON_ARRAY,
+    BUFFER_ARRAY,
+    JSON_STRING,
+    JSON_STRING_ARRAY,
+    INVALID,
+  },
+};
